8228be4bc21687c35d78576a6c055a11b3f60e7f
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-14 20:00:11 +0000

    create character counter function in a separate .js file, add composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
  console.log('hi from the char.counter!')
-});
 
-$('#textarea').on('keypress'){
-  console.log('typing, mousing, and else!')
-}
+
+ $('.new-tweet form textarea').on('keydown', function(){
+  const allowedChar = 140;
+  // // catach the count and put it in the currentChar
+  let currentChar = $(this).val().length;
+
+
+  if(currentChar < allowedChar){
+    console.log(currentChar)
+
+  } else {
+    console.log(0 - currentChar)
+  }
+
+  })
+  })
+
+
+
+
+
+
+
