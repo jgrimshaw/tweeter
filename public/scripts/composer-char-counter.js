91115a724687264ffabed2b698fc83ae8de18202
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-14 20:56:04 +0000

    counter showing red when count is lower than 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,25 +2,22 @@ $(document).ready(function() {
  console.log('hi from the char.counter!')
 
 
- $('.new-tweet form textarea').on('keydown', function(){
-  const allowedChar = 140;
-  // // catach the count and put it in the currentChar
-  let currentChar = $(this).val().length;
-
-
-  if(currentChar < allowedChar){
-    console.log(currentChar)
-
-  } else {
-    console.log(0 - currentChar)
-  }
-
-  })
-  })
-
-
-
-
+  $('.new-tweet form textarea').on('keydown', function(){
+    const allowedChar = 140;
+    //catach the count and put it in the currentChar
+    let currentChar = ($(this).val().length);
+    if(currentChar > allowedChar){
+      $('#textarea').addClass("overlimit");
+    } else {
+      $('#textarea').removeClass("overlimit");
+    }
+    let charLeft = allowedChar - $(this).val().length;
+      $('output').text(charLeft);
+      if(charLeft <= 0){
+        $('output').addClass("overlimit")
+      }
+    })
+  });
 
 
 
