1aba2089c45d6c9a76eb27d17eae259a23fbb831
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-15 20:00:28 +0000

    clean up, delete few unnecessary html tags

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 const data = [
   {
@@ -30,7 +25,7 @@ const data = [
       },
       "handle": "@rd" },
     "content": {
-      "text": "Je pense , donc je suis"
+      "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461113959088
   },
@@ -45,76 +40,48 @@ const data = [
       "handle": "@johann49"
     },
     "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461113796368
   }
 ];
 
-
-
-// // Test / driver code (temporary)
-// console.log(tweet); // to see what it looks like
-// $('#tweets-container').append(tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
-  // loops through tweets
-  function renderTweets(data){
-    for (var i = 0; i < data.length; i++){
-        let $newTweet = createTweetElement(data[i]);
-        $('#tweets-container').append($newTweet);
-    }
-  }
-
-
-
-
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-
-
-
-$(document).ready(renderTweets(data));
-
-
 function createTweetElement (data){
-  console.log('works or not?!');
-
     let username = data.user.name;
     let avatar = data.user.avatars.small;
     let userId = data.user.handle;
     let tweetText = data.content.text;
     let time = data.created_at;
 
-    let $tweet = $("#container").append(`
-    <section id="container">
-        <article class="tw-article">
-          <header class="tw-header">
+    let $tweet = $(".tweets-container").append(`
+
+      <article class="tw-article">
+        <header class="tw-header">
           <img class="tw-avatar" src=${avatar}>
-
-          <div class="tw-header-items">
-            <h2 class="tw-h2">${username}</h2>
-            <span class="tw-name">${userId}</span>
-          </div>
-
-          </header>
+          <h2 class="tw-h2">${username}</h2>
+          <span class="tw-name">${userId}</span>
+        </header>
             <p class="tw-p">${tweetText}</p>
-          <footer class="tw-footer">
-            <div>${time}</div>
-            <span class="icons"><i class="fas fa-flag">&nbsp</i><i class="fas fa-retweet">&nbsp</i><i class="fas fa-heart"></i><div>
-          </footer>
-        </article>
-      </section>
-    `);
-    return $tweet;
+        <footer class="tw-footer">
+          <time>${time}</time>
+          <span class="icons"><i class="fas fa-flag">&nbsp</i><i class="fas fa-retweet">&nbsp</i><i class="fas fa-heart"></i><div>
+        </footer>
+      </article>
+
+  `);
+  return $tweet;
+}
+
+$(document).ready(renderTweets(data));
+
+function renderTweets(data){
+  for(let i = 0; i < data.length; i++){
+      let $newTweet = createTweetElement(data[i]);
+      $(".tweets-container").append($newTweet);
+  }
 }
 
 
 
 
-var $tweet = createTweetElement(data);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweet-container').append($tweet)
