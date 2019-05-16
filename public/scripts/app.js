8f0f924c2628a7dd19bdb580e9631f9136242ea5
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-16 13:15:30 +0000

    added compose button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,4 @@
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
-
 function createTweetElement (data){
     let username = data.user.name;
     let avatar = data.user.avatars.small;
@@ -75,7 +27,7 @@ function createTweetElement (data){
 
 function renderTweets(data){
   for(let i = 0; i < data.length; i++){
-      let $newTweet = createTweetElement(data[i]);
+      let $newTweet = createTweetElement(data[i]).text;
       $(".tweets-container").append($newTweet);
   }
 }
@@ -83,22 +35,32 @@ function renderTweets(data){
 $(document).ready($(function() {
     let $form = $('.tweet-form');
     $form.on('submit', function (event) {
-        event.preventDefault();
-        let tweetText = $('textarea').val();
-        console.log(tweetText.length);
-        if( tweetText.length > 140 || tweetText.length <= 0){
-            alert("Your tweet is too long or too short!");
-        } else {
-            console.log('submiting tweet...');
-            $.getJSON({
-                url: 'http://localhost:8080/tweets',
-                method: 'GET',
-                data: {get_param: 'value'},
-                success: function (data) {
-                    renderTweets(data);
-                    console.log('Success!');
-                }
-            })
-        }
-    });
+      event.preventDefault();
+      let tweetText = $('textarea').val();
+      console.log(tweetText.length);
+      if( tweetText.length > 140 || tweetText.length <= 0){
+          alert("Your tweet is too long or too short!");
+      } else {
+          console.log('submiting tweet...');
+          $.post({
+            url: 'http://localhost:8080/tweets',
+            method: 'POST',
+            data: {text: tweetText},
+            success: function(data){
+              $('.newTweet').after(createTweetElement(data));
+              $('.container').text
+            }
+          })
+      }
+});
+
+$.getJSON({
+  url: 'http://localhost:8080/tweets',
+  method: 'GET',
+  data: {get_param: 'value'},
+  success: function (data) {
+  renderTweets(data);
+  console.log('Success!');
+  }
+  });
 }))
